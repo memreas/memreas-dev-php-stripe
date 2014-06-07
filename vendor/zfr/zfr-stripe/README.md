a94a36296e014693b1aaad10b6d092cafd5e1c7f
john meah <johnmeah@memreas.com>
2014-06-07 12:42:12 -0400

    7-JUN-2014 updated composer.phar

---
+++ vendor/zfr/zfr-stripe/README.md
@@ -0,0 +1,290 @@
+ZfrStripe
+=========
+
+[![Latest Stable Version](https://poser.pugx.org/zfr/zfr-stripe/v/stable.png)](https://packagist.org/packages/zfr/zfr-stripe)
+
+ZfrStripe is a modern PHP library based on Guzzle for [Stripe payment system](https://www.stripe.com).
+
+> Note : this library does not contain tests, mainly because I'm not sure about how to write tests for an API
+wrapper. Don't hesitate to help on this ;-).
+
+## Dependencies
+
+* [Guzzle](http://www.guzzlephp.org): >= 3.6
+
+## Installation
+
+Installation of ZfrStripe is only officially supported using Composer:
+
+```sh
+php composer.phar require zfr/zfr-stripe:1.*
+```
+
+## Tutorial
+
+First, you need to instantiate the Stripe client, passing your private API key (you can find it in your Stripe
+settings):
+
+```php
+$client = new StripeClient('my-api-key');
+```
+
+> You can change the API key for the client using the `setApiKey` method. This is useful if you are using Stripe
+Connect and make both your own API calls and API calls on behalf of your users.
+
+The currently latest supported version of the API is 2014-03-28. You can (and should) also explicitly specify the version
+of the client using the second parameter:
+
+```php
+$client = new StripeClient('my-api-key', '2014-03-28');
+```
+
+### Versioning
+
+Stripe versions its API using a dated version ("2013-12-03", "2014-01-31"...). Their [versioning policy](https://stripe.com/docs/upgrades)
+is to release a new dated version each time something changes in their API (for instance, if a response returns
+new attributes, if a property is marked as deprecated)...
+
+ZfrStripe will follow this scheming method by releasing a new service description.
+
+By default, each new minor version (1.1.0 to 1.2.0 for instance) *may* update the service descriptor to the latest
+available. This means that to keep BC you should either tighten your dependency (1.3.* instead of 1.* for instance)
+OR always specify the exact service descriptor you want, as shown above.
+
+You can know about the available descriptors in the `ZfrStripe\Client\ServiceDescription` folder.
+
+### How to use it?
+
+Using the client is easy. For instance, here is how you'd create a new charge:
+
+```php
+$details = $client->createCharge(array(
+    'amount'   => 500,
+    'currency' => 'EUR',
+    'customer' => 'cus_37EGGf4LMGgYy8'
+));
+```
+
+The parameters have a direct one-to-one mapping with the official documentation (for any reference, you can also
+check the `ZfrStripe\Client\ServiceDescription\Stripe-2014-01-31.php` file). To know what the responses look like, please
+refer to the [official API reference](https://stripe.com/docs/api).
+
+#### Expand
+
+All Stripe API requests have support to expand some nested objects inside responses. Therefore, ZfrStripe supports this
+through the `expand` parameter, which must be an array:
+
+```php
+$details = $client->getCharges(array(
+    'expand' => array('customer')
+));
+```
+
+#### Iterators
+
+You may want to retrieve a list of customers, events or charges. Instead of manually doing all the requests yourself,
+you can use iterators. ZfrStripe provides iterators for all iterable resources:
+
+```php
+$iterator = $client->getCustomersIterator();
+
+foreach ($iterator as $user) {
+    // Do something
+}
+```
+
+By default, ZfrStripe retrieves 100 elements per API call (which is the maximum allowed by Stripe API). You may want
+to lower this limit by using the `setPageSize` method. You can also set a upper bound of how many results you want
+to retrieve by using the `setLimit` method.
+
+Finally, you can still use API parameters when using an iterator. For instance, this will retrieve all the events
+that have the event `customer.subscription.updated`, doing a new API call each 50 elements (this means that only
+up to 50 elements are stored in memory), setting a limit of maximum 500 elements to retrieve:
+
+```php
+$iterator = $client->getEventsIterator(array('type' => 'customer.subscription.updated'));
+$iterator->setPageSize(50);
+$iterator->setLimit(500);
+
+foreach ($iterator as $event) {
+    // Do something
+}
+```
+
+> Note: starting from 2014-03-28 version, Stripe has changed how it handles pagination, and now uses a cursor-based
+pagination. This is much more flexible, as it allows you to keep track of your pagination, even if new items were
+added while you paginate data. Furthermore, it has a new property that allows us to avoid doing an additional request
+at the end to check if there are more data to fetch:
+
+```php
+$iterator = $client->getInvoicesIterator(['starting_after' => 'in_abcdef');
+
+foreach ($iterator as $invoices) {
+    // Do something
+}
+```
+
+ZfrStripe takes care of fetching the last item in the batch, extracting the id, and continuing doing requests
+until no more data is available!
+
+### Exceptions
+
+ZfrStripe tries its best to throw useful exceptions. Two kinds of exceptions can occur:
+
+* Guzzle exceptions: by default, Guzzle automatically validate parameters according to rules defined in the
+ service description **before** sending the actual request. If you encounter those exceptions, you likely have broken
+ code.
+* ZfrStripe exceptions: those exceptions are thrown if an error occurred on Stripe side. Each exception implement
+`ZfrStripe\Exception\ExceptionInterface`.
+
+Here are all the exceptions:
+
+* `ZfrStripe\Exception\UnauthorizedException`: your API key is likely to be wrong...
+* `ZfrStripe\Exception\ApiRateLimitException`: occurs when you have reached the Stripe API limit.
+* `ZfrStripe\Exception\BadRequestException`: occurs for 400 error code.
+* `ZfrStripe\Exception\CardErrorException`: occurs for any card errors. According to Stripe, this error happen when
+parameters were valid but the request failed (for instance if a card CVC is invalid).
+* `ZfrStripe\Exception\NotFoundException`: is thrown whenever client receives a 404 exception.
+* `ZfrStripe\Exception\RequestFailedException`: occurs for generic 402 error.
+* `ZfrStripe\Exception\ServerErrorException`: any errors where Stripe is likely to be doing something wrong...
+
+Usage:
+
+```php
+use ZfrStripe\Exception\TransactionErrorException;
+
+try {
+    $client->createCard(array(
+        'card' => array(
+            'number'    => '4242424242424242',
+            'exp_month' => '01',
+            'exp_year'  => '2016'
+        )
+    ));
+} catch (CardErrorException $exception) {
+    // Seems we couldn't create the card, maybe because it's invalid
+    $why = $exception->getMessage();
+
+    // Let's also get the response to have more info:
+    $response = $exception->getResponse();
+} catch (\Exception $exception) {
+    // Catch any other exception...
+}
+```
+
+### Complete reference
+
+Here is a complete list of all methods:
+
+CHARGE RELATED METHODS:
+
+* array captureCharge(array $args = array())
+* array createCharge(array $args = array())
+* array getCharge(array $args = array())
+* array getCharges(array $args = array())
+* array refundCharge(array $args = array())
+* array updateCharge(array $args = array())
+
+CUSTOMER RELATED METHODS:
+
+* array createCustomer(array $args = array())
+* array deleteCustomer(array $args = array())
+* array getCustomer(array $args = array())
+* array getCustomers(array $args = array())
+* array updateCustomer(array $args = array())
+
+CARD RELATED METHODS:
+
+* array createCard(array $args = array())
+* array deleteCard(array $args = array())
+* array getCard(array $args = array())
+* array getCards(array $args = array())
+* array updateCard(array $args = array())
+
+SUBSCRIPTION RELATED METHODS:
+
+* array cancelSubscription(array $args = array())
+* array createSubscription(array $args = array())
+* array getSubscription(array $args = array())
+* array getSubscriptions(array $args = array())
+* array updateSubscription(array $args = array())
+
+PLAN RELATED METHODS:
+
+* array createPlan(array $args = array())
+* array deletePlan(array $args = array())
+* array getPlan(array $args = array())
+* array getPlans(array $args = array())
+* array updatePlan(array $args = array())
+
+COUPON RELATED METHODS:
+
+* array createCoupon(array $args = array())
+* array deleteCoupon(array $args = array())
+* array getCoupon(array $args = array())
+* array getCoupons(array $args = array())
+
+DISCOUNT RELATED METHODS:
+
+* array deleteCustomerDiscount(array $args = array())
+* array deleteSubscriptionDiscount(array $args = array())
+
+INVOICE RELATED METHODS:
+
+* array createInvoice(array $args = array())
+* array getInvoice(array $args = array())
+* array getInvoiceLineItems(array $args = array())
+* array getInvoices(array $args = array())
+* array getUpcomingInvoice(array $args = array())
+* array payInvoice(array $args = array())
+* array updateInvoice(array $args = array())
+
+INVOICE ITEM RELATED METHODS:
+
+* array createInvoiceItem(array $args = array())
+* array deleteInvoiceItem(array $args = array())
+* array getInvoiceItem(array $args = array())
+* array getInvoiceItems(array $args = array())
+* array updateInvoiceItem(array $args = array())
+
+DISPUTE RELATED METHODS:
+
+* array closeDispute(array $args = array())
+* array updateDispute(array $args = array())
+
+TRANSFER RELATED METHODS:
+
+* array cancelTransfer(array $args = array())
+* array createTransfer(array $args = array())
+* array getTransfer(array $args = array())
+* array getTransfers(array $args = array())
+* array updateTransfer(array $args = array())
+
+RECIPIENT RELATED METHODS:
+
+* array createRecipient(array $args = array())
+* array deleteRecipient(array $args = array())
+* array getRecipient(array $args = array())
+* array getRecipients(array $args = array())
+* array updateRecipient(array $args = array())
+
+APPLICATION FEE RELATED METHODS:
+
+* array getApplicationFee(array $args = array())
+* array getApplicationFees(array $args = array())
+* array refundApplicationFee(array $args = array())
+
+TOKEN RELATED METHODS:
+
+* array createCardToken(array $args = array())
+* array createBankAccountToken(array $args = array())
+* array getToken(array $args = array())
+
+EVENT RELATED METHODS:
+
+* array getEvent(array $args = array())
+* array getEvents(array $args = array())
+
+ACCOUNT RELATED METHODS:
+
+* array getAccount(array $args = array())
---
+++ vendor/zfr/zfr-stripe/src/ZfrStripe/Client/StripeClient.php
@@ -0,0 +1,286 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace ZfrStripe\Client;
+
+use Guzzle\Common\Event;
+use Guzzle\Plugin\ErrorResponse\ErrorResponsePlugin;
+use Guzzle\Service\Client;
+use Guzzle\Service\Description\ServiceDescription;
+use Guzzle\Service\Resource\ResourceIterator;
+use ZfrStripe\Client\Iterator\StripeCommandsCursorIterator;
+use ZfrStripe\Client\Iterator\StripeCommandsIterator;
+use ZfrStripe\Http\QueryAggregator\StripeQueryAggregator;
+
+/**
+ * @author  Michaël Gallego <mic.gallego@gmail.com>
+ * @licence MIT
+ *
+ * CHARGE RELATED METHODS:
+ *
+ * @method array captureCharge(array $args = array()) {@command Stripe CaptureCharge}
+ * @method array createCharge(array $args = array()) {@command Stripe CreateCharge}
+ * @method array getCharge(array $args = array()) {@command Stripe GetCharge}
+ * @method array getCharges(array $args = array()) {@command Stripe GetCharges}
+ * @method array refundCharge(array $args = array()) {@command Stripe RefundCharge}
+ * @method array updateCharge(array $args = array()) {@command Stripe UpdateCharge}
+ *
+ * CUSTOMER RELATED METHODS:
+ *
+ * @method array createCustomer(array $args = array()) {@command Stripe CreateCustomer}
+ * @method array deleteCustomer(array $args = array()) {@command Stripe DeleteCustomer}
+ * @method array getCustomer(array $args = array()) {@command Stripe GetCustomer}
+ * @method array getCustomers(array $args = array()) {@command Stripe GetCustomers}
+ * @method array updateCustomer(array $args = array()) {@command Stripe UpdateCustomer}
+ *
+ * CARD RELATED METHODS:
+ *
+ * @method array createCard(array $args = array()) {@command Stripe CreateCard}
+ * @method array deleteCard(array $args = array()) {@command Stripe DeleteCard}
+ * @method array getCard(array $args = array()) {@command Stripe GetCard}
+ * @method array getCards(array $args = array()) {@command Stripe GetCards}
+ * @method array updateCard(array $args = array()) {@command Stripe UpdateCard}
+ *
+ * SUBSCRIPTION RELATED METHODS:
+ *
+ * @method array cancelSubscription(array $args = array()) {@command Stripe CancelSubscription}
+ * @method array createSubscription(array $args = array()) {@command Stripe CreateSubscription}
+ * @method array getSubscription(array $args = array()) {@command Stripe GetSubscription}
+ * @method array getSubscriptions(array $args = array()) {@command Stripe GetSubscriptions}
+ * @method array updateSubscription(array $args = array()) {@command Stripe UpdateSubscription}
+ *
+ * PLAN RELATED METHODS:
+ *
+ * @method array createPlan(array $args = array()) {@command Stripe CreatePlan}
+ * @method array deletePlan(array $args = array()) {@command Stripe DeletePlan}
+ * @method array getPlan(array $args = array()) {@command Stripe GetPlan}
+ * @method array getPlans(array $args = array()) {@command Stripe GetPlans}
+ * @method array updatePlan(array $args = array()) {@command Stripe UpdatePlan}
+ *
+ * COUPON RELATED METHODS:
+ *
+ * @method array createCoupon(array $args = array()) {@command Stripe CreateCoupon}
+ * @method array deleteCoupon(array $args = array()) {@command Stripe DeleteCoupon}
+ * @method array getCoupon(array $args = array()) {@command Stripe GetCoupon}
+ * @method array getCoupons(array $args = array()) {@command Stripe GetCoupons}
+ *
+ * DISCOUNT RELATED METHODS:
+ *
+ * @method array deleteCustomerDiscount(array $args = array()) {@command Stripe DeleteCustomerDiscount}
+ * @method array deleteSubscriptionDiscount(array $args = array()) {@command Stripe DeleteSubscriptionDiscount}
+ *
+ * INVOICE RELATED METHODS:
+ *
+ * @method array createInvoice(array $args = array()) {@command Stripe CreateInvoice}
+ * @method array getInvoice(array $args = array()) {@command Stripe GetInvoice}
+ * @method array getInvoiceLineItems(array $args = array()) {@command Stripe GetInvoiceLineItems}
+ * @method array getInvoices(array $args = array()) {@command Stripe GetInvoices}
+ * @method array getUpcomingInvoice(array $args = array()) {@command Stripe GetUpcomingInvoice}
+ * @method array payInvoice(array $args = array()) {@command Stripe PayInvoice}
+ * @method array updateInvoice(array $args = array()) {@command Stripe UpdateInvoice}
+ *
+ * INVOICE ITEM RELATED METHODS:
+ *
+ * @method array createInvoiceItem(array $args = array()) {@command Stripe CreateInvoiceItem}
+ * @method array deleteInvoiceItem(array $args = array()) {@command Stripe DeleteInvoiceItem}
+ * @method array getInvoiceItem(array $args = array()) {@command Stripe GetInvoiceItem}
+ * @method array getInvoiceItems(array $args = array()) {@command Stripe GetInvoiceItems}
+ * @method array updateInvoiceItem(array $args = array()) {@command Stripe UpdateInvoiceItem}
+ *
+ * DISPUTE RELATED METHODS:
+ *
+ * @method array closeDispute(array $args = array()) {@command Stripe CloseDispute}
+ * @method array updateDispute(array $args = array()) {@command Stripe UpdateDispute}
+ *
+ * TRANSFER RELATED METHODS:
+ *
+ * @method array cancelTransfer(array $args = array()) {@command Stripe CancelTransfer}
+ * @method array createTransfer(array $args = array()) {@command Stripe CreateTransfer}
+ * @method array getTransfer(array $args = array()) {@command Stripe GetTransfer}
+ * @method array getTransfers(array $args = array()) {@command Stripe GetTransfers}
+ * @method array updateTransfer(array $args = array()) {@command Stripe UpdateTransfer}
+ *
+ * RECIPIENT RELATED METHODS:
+ *
+ * @method array createRecipient(array $args = array()) {@command Stripe CreateRecipient}
+ * @method array deleteRecipient(array $args = array()) {@command Stripe DeleteRecipient}
+ * @method array getRecipient(array $args = array()) {@command Stripe GetRecipient}
+ * @method array getRecipients(array $args = array()) {@command Stripe GetRecipients}
+ * @method array updateRecipient(array $args = array()) {@command Stripe UpdateRecipient}
+ *
+ * APPLICATION FEE RELATED METHODS:
+ *
+ * @method array getApplicationFee(array $args = array()) {@command Stripe GetApplicationFee}
+ * @method array getApplicationFees(array $args = array()) {@command Stripe GetApplicationFees}
+ * @method array refundApplicationFee(array $args = array()) {@command Stripe RefundApplicationFee}
+ *
+ * TOKEN RELATED METHODS:
+ *
+ * @method array createCardToken(array $args = array()) {@command Stripe CreateCardToken}
+ * @method array createBankAccountToken(array $args = array()) {@command Stripe CreateBankAccountToken}
+ * @method array getToken(array $args = array()) {@command Stripe GetToken}
+ *
+ * EVENT RELATED METHODS:
+ *
+ * @method array getEvent(array $args = array()) {@command Stripe GetEvent}
+ * @method array getEvents(array $args = array()) {@command Stripe GetEvents}
+ *
+ * ACCOUNT RELATED METHODS:
+ *
+ * @method array getAccount(array $args = array()) {@command Stripe GetAccount}
+ *
+ * ITERATOR METHODS:
+ *
+ * @method ResourceIterator getCustomersIterator()
+ * @method ResourceIterator getChargesIterator()
+ * @method ResourceIterator getCardsIterator()
+ * @method ResourceIterator getSubscriptionsIterator()
+ * @method ResourceIterator getPlansIterator()
+ * @method ResourceIterator getCouponsIterator()
+ * @method ResourceIterator getInvoicesIterator()
+ * @method ResourceIterator getInvoiceItemsIterator()
+ * @method ResourceIterator getTransfersIterator()
+ * @method ResourceIterator getRecipientsIterator()
+ * @method ResourceIterator getApplicationFeesIterator()
+ * @method ResourceIterator getEventsIterator()
+ */
+class StripeClient extends Client
+{
+    /**
+     * Stripe API version
+     */
+    const LATEST_API_VERSION = '2014-03-28';
+
+    /**
+     * @var string
+     */
+    protected $apiKey;
+
+    /**
+     * Constructor
+     *
+     * @param string $apiKey
+     * @param string $version
+     */
+    public function __construct($apiKey, $version = self::LATEST_API_VERSION)
+    {
+        parent::__construct();
+
+        $this->apiKey = (string) $apiKey;
+
+        $this->setDescription(ServiceDescription::factory(sprintf(
+            __DIR__ . '/ServiceDescription/Stripe-%s.php',
+            (string) $version
+        )));
+
+        // Prefix the User-Agent by SDK version
+        $this->setUserAgent('zfr-stripe-php', true);
+
+        // Add an event to set the Authorization param
+        $dispatcher = $this->getEventDispatcher();
+
+        $dispatcher->addSubscriber(new ErrorResponsePlugin());
+        $dispatcher->addListener('command.after_prepare', array($this, 'afterPrepare'));
+        $dispatcher->addListener('command.before_send', array($this, 'authorizeRequest'));
+    }
+
+    /**
+     * Override the API key
+     *
+     * @param  string $apiKey
+     * @return void
+     */
+    public function setApiKey($apiKey)
+    {
+        $this->apiKey = (string) $apiKey;
+    }
+
+    /**
+     * Get the current API key
+     *
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function __call($method, $args = array())
+    {
+        if (substr($method, -8) === 'Iterator') {
+            // Allow magic method calls for iterators (e.g. $client-><CommandName>Iterator($params))
+            $commandOptions  = isset($args[0]) ? $args[0] : array();
+            $iteratorOptions = isset($args[1]) ? $args[1] : array();
+            $command         = $this->getCommand(substr($method, 0, -8), $commandOptions);
+
+            // Based on the API version, we use (or not) the new cursor based pagination
+            if ($this->getApiVersion() >= '2014-03-28') {
+                return new StripeCommandsCursorIterator($command, $iteratorOptions);
+            } else {
+                return new StripeCommandsIterator($command, $iteratorOptions);
+            }
+        }
+
+        return parent::__call(ucfirst($method), $args);
+    }
+
+    /**
+     * Get current Stripe API version
+     *
+     * @return string
+     */
+    public function getApiVersion()
+    {
+        return $this->serviceDescription->getApiVersion();
+    }
+
+    /**
+     * Modify the query aggregator
+     *
+     * @internal
+     * @param  Event $event
+     * @return void
+     */
+    public function afterPrepare(Event $event)
+    {
+        /* @var \Guzzle\Service\Command\CommandInterface $command */
+        $command = $event['command'];
+        $request = $command->getRequest();
+
+        $request->getQuery()->setAggregator(new StripeQueryAggregator());
+    }
+
+    /**
+     * Authorize the request
+     *
+     * @internal
+     * @param  Event $event
+     * @return void
+     */
+    public function authorizeRequest(Event $event)
+    {
+        /* @var \Guzzle\Service\Command\CommandInterface $command */
+        $command = $event['command'];
+
+        $request = $command->getRequest();
+        $request->setAuth($this->apiKey);
+    }
+}
