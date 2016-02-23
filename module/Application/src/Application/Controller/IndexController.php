889b9cb7b9af6138b320ac8708a9789d58082bad
john meah <johnmeah@memreas.com>
2016-02-22 22:21:55 -0500

    .added code to debug emails

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -20,11 +20,36 @@ use Application\memreas\AWSMemreasRedisCache;
 use Application\memreas\AWSMemreasRedisSessionHandler;
 
 class IndexController extends AbstractActionController {
-	
+	private $aws;
 	public function indexAction() {
+		ob_start ();
+		$actionname = isset ( $_REQUEST ["action"] ) ? $_REQUEST ["action"] : '';
+		if ($actionname == "clearlog") {
+			unlink ( getcwd () . '/php_errors.log' );
+			error_log ( "Log has been cleared!" );
+			echo '<pre>' . file_get_contents ( getcwd () . '/php_errors.log' );
+			// End buffering and flush
+			ob_end_clean ();
+			exit ();
+		} else if ($actionname == "showlog") {
+			$result = '<pre>' . file_get_contents ( getcwd () . '/php_errors.log' );
+			echo $result;
+			// End buffering and flush
+			ob_end_flush ();
+			exit ();
+		} else if ($actionname == "email") {
+			$to = $_REQUEST ["to"];
+			$subject = $_REQUEST ["subject"];
+			$content = $_REQUEST ["content"];
+			
+			$this->aws = MemreasConstants::fetchAWS();
+			$this->aws->sendSeSMail ( array (
+					$to 
+			), $subject, $content );
+		} else {
 			$view = new ViewModel ();
 			$view->setTemplate ( 'application/error/500.phtml' );
 			return $view;
+		}
 	}
-	
 } // end class IndexController
