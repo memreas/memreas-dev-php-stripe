5fd9d7f59e98aa0861754556ac7233ae179d436d
john meah <johnmeah@memreas.com>
2016-02-24 17:17:18 -0500

    .

--- module/Application/src/Application/memreas/CheckGitPull.php
+++ module/Application/src/Application/memreas/CheckGitPull.php
@@ -22,65 +22,47 @@ class CheckGitPull {
 		return $output;
 	}
 	public function exec($pull = false) {
-		$cm = __CLASS__.__METHOD__;
-		Mlog::addone ( $cm , __LINE__);
 		$pulled_latest = false;
 		$output = '';
-		Mlog::addone ( $cm , __LINE__);
 		if (file_exists ( $this->gitlock ) && ! $pull) {
 			$pulled_latest = true;
 		} else if (! file_exists ( $this->gitlock ) || $pull) {
 			// Setup SSH agent
-		Mlog::addone ( $cm , __LINE__);
 			$output = $this->execOps ( 'eval "$(ssh-agent -s)"' );
 			
 			// Add key
-		Mlog::addone ( $cm , __LINE__);
 			$output .= $this->execOps ( "ssh-add ~/.ssh/id_rsa" );
 			
 			// check ssh auth sock
-		Mlog::addone ( $cm , __LINE__);
 			$output .= $this->execOps ( 'echo "$SSH_AUTH_SOCK"' );
 			
 			// check github access
-		Mlog::addone ( $cm , __LINE__);
 			$output .= $this->execOps ( 'ssh -T git@github.com' );
 			
 			// cd to $github_basedir
-		Mlog::addone ( $cm , __LINE__);
 			$output .= $this->execOps ( "cd $this->github_basedir" );
 			
 			// remove composer.phar 10-JAN-2015 - pushing entire folder to avoid merge conflicts
 			// $output .= $this->execOps ( "git reset --hard HEAD" );
 			
 			// git pull
-		Mlog::addone ( $cm , __LINE__);
 			$output .= $this->execOps ( "git pull" );
 			
 			// write lock file
-		Mlog::addone ( $cm , __LINE__);
 			if (file_exists ( $this->gitlock )) {
-		Mlog::addone ( $cm , __LINE__);
 				$output .= $this->execOps ( "rm " . $this->gitlock );
 			}
-		Mlog::addone ( $cm , __LINE__);
 			$file = fopen ( $this->gitlock, "w" );
-		Mlog::addone ( $cm , __LINE__);
 			echo fwrite ( $file, $output );
-		Mlog::addone ( $cm , __LINE__);
 			fclose ( $file );
 			
 			// set permissions
-		Mlog::addone ( $cm , __LINE__);
 			$pulled_latest = true;
 			
-		Mlog::addone ( $cm , __LINE__);
 			$output .= "\nCompleted git pull op...";
 			
-		Mlog::addone ( $cm , __LINE__);
 			Mlog::addone ( 'output::', $output );
 		}
-		Mlog::addone ( $cm , __LINE__);
 		return $output;
 	}
 } // end class MemreasTranscoder
