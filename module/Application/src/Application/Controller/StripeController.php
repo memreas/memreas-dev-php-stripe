ddd785cac8001eafa4e69c5d424d5c98ff1d9109
Tran Tuan <tranit1209@gmail.com>
2016-03-01 15:38:56 +0700

    ..

--- module/Application/src/Application/Controller/StripeController.php
+++ module/Application/src/Application/Controller/StripeController.php
@@ -387,7 +387,7 @@ class StripeController extends AbstractActionController {
 			Mlog::addone ( __CLASS__ . __METHOD__ . '$json-->', $json );
 			$message_data = json_decode ( $json, true );
 			$MemreasStripe = new MemreasStripe ( $this->getServiceLocator (), $this->aws );
-			$this->flushResponse ( json_encode ( $MemreasStripe->checkOwnEvent ( message_data, true ) ) );
+			$this->flushResponse ( json_encode ( $MemreasStripe->checkOwnEvent ( $message_data, true ) ) );
 			die ();
 		}
 	}
