10492daf2fd2bbed5d783929d6715d4852dc383e
Tran Tuan <tranit1209@gmail.com>
2016-03-02 16:42:01 +0700

    ..

--- module/Application/src/Application/memreas/MemreasStripe.php
+++ module/Application/src/Application/memreas/MemreasStripe.php
@@ -1277,6 +1277,7 @@ class StripeInstance {
 			 * Send Purchase Confirmation email
 			 */
 			$buyer = $this->memreasStripeTables->getUserTable ()->getUser ( $data['user_id'] );
+			echo '<pre>'; print_r ($buyer); die();
 			$viewModel = new ViewModel ( array (
 				'username' => $buyer->first_name . ' ' . $buyer->last_name,
 				'seller_name' => $seller_account->firstname . ' ' . $seller_account->last_name,
