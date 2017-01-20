7ee11d06e750d6304b655a9df70898ffd2cf7411
john meah <johnmeah@memreas.com>
2017-01-20 13:22:50 -0500

    updated redis ip

--- module/Application/src/Application/Model/MemreasConstants.localhost.php
+++ module/Application/src/Application/Model/MemreasConstants.localhost.php
@@ -10,7 +10,6 @@ namespace Application\Model;
 class MemreasConstants {
 	
 	// Redis section ubuntu standalone for redis 3 version
-	// const REDIS_SERVER_ENDPOINT = "54.225.187.57";
 	const REDIS_SERVER_ENDPOINT = "127.0.0.1";
 	const REDIS_SERVER_USE = true;
 	const REDIS_SERVER_SESSION_ONLY = true;
